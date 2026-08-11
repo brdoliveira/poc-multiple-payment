208fb7c3186a8c688e7a8f75b11664f69e64284d
brdoliveira <brdoliveira@users.noreply.github.com>
2026-08-11 13:23:32 -0300

    chore: update infra/aws/terraform/documentdb.tf

--- infra/aws/terraform/documentdb.tf
+++ infra/aws/terraform/documentdb.tf
@@ -6,21 +6,21 @@ resource "aws_docdb_subnet_group" "main" {
 }
 
 resource "aws_docdb_cluster" "main" {
-  cluster_identifier       = "${local.name}-documentdb"
-  engine                   = "docdb"
-  engine_version           = "5.0"
-  master_username          = var.documentdb_username
-  master_password          = random_password.documentdb.result
-  port                     = var.documentdb_port
-  db_subnet_group_name     = aws_docdb_subnet_group.main.name
-  vpc_security_group_ids   = [aws_security_group.documentdb.id]
-  storage_encrypted        = true
-  kms_key_id               = aws_kms_key.main.arn
-  backup_retention_period  = 7
-  preferred_backup_window  = "03:00-04:00"
-  skip_final_snapshot      = var.environment != "prod"
-  deletion_protection      = var.environment == "prod"
-  apply_immediately        = true
+  cluster_identifier      = "${local.name}-documentdb"
+  engine                  = "docdb"
+  engine_version          = "5.0"
+  master_username         = var.documentdb_username
+  master_password         = random_password.documentdb.result
+  port                    = var.documentdb_port
+  db_subnet_group_name    = aws_docdb_subnet_group.main.name
+  vpc_security_group_ids  = [aws_security_group.documentdb.id]
+  storage_encrypted       = true
+  kms_key_id              = aws_kms_key.main.arn
+  backup_retention_period = 7
+  preferred_backup_window = "03:00-04:00"
+  skip_final_snapshot     = var.environment != "prod"
+  deletion_protection     = var.environment == "prod"
+  apply_immediately       = true
 
   tags = { Name = "${local.name}-documentdb" }
 }
